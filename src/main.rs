7af7e67143442d9afc40b0047b00185e1acd800f
CatThingy <andyc.andychen@gmail.com>
2022-11-02 22:17:09 -0700

    feat: scaffold layout manager

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,97 @@
+use river_layout_v3::{Event, RiverLayoutManagerV3};
+
+use wayland_client::{
+    global_filter,
+    protocol::wl_output::{self, WlOutput},
+    Display, GlobalManager, Main,
+};
+
+mod river_layout_v3;
+
+pub struct Globals {
+    pub namespace: String,
+    pub layout_manager: Option<Main<RiverLayoutManagerV3>>,
+}
+
+pub struct Output {
+    pub output: Main<WlOutput>,
+    pub view_padding: i32,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let display = Display::connect_to_env().unwrap();
+    let mut event_queue = display.create_event_queue();
+
+    let attached_display = (*display).clone().attach(event_queue.token());
+
+    let mut globals = Globals {
+        namespace: "vallis".to_owned(),
+        layout_manager: None,
+    };
+
+    GlobalManager::new_with_cb(
+        &attached_display,
+        global_filter!(
+            [
+                RiverLayoutManagerV3,
+                1,
+                |layout_manager: Main<RiverLayoutManagerV3>, mut globals: DispatchData| {
+                    globals.get::<Globals>().unwrap().layout_manager = Some(layout_manager);
+                }
+            ],
+            [
+                WlOutput,
+                3,
+                |output: Main<WlOutput>, _globals: DispatchData| {
+                    output.quick_assign(move |output, event, mut globals| match event {
+                        wl_output::Event::Done => {
+                            let output = Output {
+                                output,
+                                view_padding: 0,
+                            };
+                            if let Some(globals) = globals.get::<Globals>() {
+                                let layout = globals
+                                    .layout_manager
+                                    .as_ref()
+                                    .expect("Compositor doesn't implement river_layout_v3")
+                                    .get_layout(&output.output, globals.namespace.clone());
+                                layout.quick_assign(move |layout, event, _| match event {
+                                    Event::NamespaceInUse => {
+                                        layout.destroy();
+                                        panic!("Namespace already in use");
+                                    }
+                                    Event::LayoutDemand {
+                                        view_count,
+                                        usable_width,
+                                        usable_height,
+                                        tags,
+                                        serial,
+                                    } => {
+                                        todo!()
+                                    }
+                                    Event::UserCommand { command } => {
+                                        todo!()
+                                    }
+                                });
+                            }
+                        }
+                        _ => {}
+                    });
+                }
+            ]
+        ),
+    );
+
+    loop {
+        event_queue
+            .dispatch(&mut globals, |event, object, _| {
+                panic!(
+                    "orphan event: {}@{}: {}",
+                    event.interface,
+                    object.as_ref().id(),
+                    event.name
+                );
+            })
+            .unwrap();
+    }
 }
--- src/river_layout_v3.rs
+++ src/river_layout_v3.rs
@@ -1,14 +1,25 @@
-pub mod client {
+// re-exports for easier use
+pub use wayland::client::{river_layout_manager_v3::RiverLayoutManagerV3, river_layout_v3::Event};
+
+pub mod wayland {
+    // The generated code tends to trigger a lot of warnings
+    // so we isolate it into a very permissive module
     #![allow(dead_code, non_camel_case_types, unused_unsafe, unused_variables)]
-    #![allow(non_upper_case_globals, non_snake_case, unused_imports)]
-    // These imports are used by the generated code
-    pub(crate) use wayland_client::protocol::*;
-    pub(crate) use wayland_client::protocol::{wl_region, wl_surface};
-    pub(crate) use wayland_client::sys;
-    pub(crate) use wayland_client::{AnonymousObject, Attached, Main, Proxy, ProxyMap};
-    pub(crate) use wayland_commons::map::{Object, ObjectMetadata};
-    pub(crate) use wayland_commons::smallvec;
-    pub(crate) use wayland_commons::wire::{Argument, ArgumentType, Message, MessageDesc};
-    pub(crate) use wayland_commons::{Interface, MessageGroup};
-    include!("../generated/river_layout_v3.rs");
+    #![allow(non_upper_case_globals, non_snake_case, unused_imports, unknown_lints)]
+    #![allow(clippy::all)]
+
+    pub mod client {
+        // These imports are used by the generated code
+        pub(crate) use wayland_client::protocol::wl_output;
+        pub(crate) use wayland_client::sys;
+        pub(crate) use wayland_client::{
+            AnonymousObject, Main, Proxy, ProxyMap,
+        };
+        pub(crate) use wayland_commons::map::{Object, ObjectMetadata};
+        pub(crate) use wayland_commons::smallvec;
+        pub(crate) use wayland_commons::wire::{Argument, ArgumentType, Message, MessageDesc};
+        pub(crate) use wayland_commons::{Interface, MessageGroup};
+
+        include!("../generated/river_layout_v3.rs");
+    }
 }
